3b6fe1c7216ef23913e7aacd3dd48e3d6fb63488
Kjnjoki <njoki19kj@gmail.com>
2023-02-16 10:07:00 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  * main - this is the main function
  *
  *Return: always 0
+ */
 
 int main(void)
 {
           puts("\"Programming is like building a multilingual puzzle");
-	  return (0):
+	  return (0);
 }
